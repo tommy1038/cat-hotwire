3be8c75d1aa94d8e05de78a99c09ed573f6607c5
Ryota Tomiyama <tomiyama.ryota@a-tm.co.jp>
2025-01-17 14:46:48 +0900

    編集のTurbo Streams化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -35,7 +35,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ねこを更新しました。", status: :see_other
+      flash.now.notice = "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
