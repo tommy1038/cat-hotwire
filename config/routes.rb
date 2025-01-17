562731ad805280d382bbbba9796fd07c00b41b0d
Ryota Tomiyama <tomiyama.ryota@a-tm.co.jp>
2025-01-17 12:53:17 +0900

    動物5種のscaffoldを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :owls
+  resources :hedgehogs
+  resources :chicks
+  resources :dogs
+  resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
